b0281e862614ad6899225fa1152b686ac385ceb4
guisantana02 <guisantana2202@gmail.com>
2025-11-27 10:51:13 -0300

    Update mux2para1.vhd

--- rtl/mux2para1.vhd
+++ rtl/mux2para1.vhd
@@ -3,12 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity mux2para1 is
     generic (
-        N : positive := 8
+        N: positive := 8
     );
     port (
-        sel : in  std_logic;
-        e0,e1  : in  std_logic_vector(N-1 downto 0);
-        y   : out std_logic_vector(N-1 downto 0)
+        sel: in  std_logic;
+        e0,e1: in  std_logic_vector(N-1 downto 0);
+        y: out std_logic_vector(N-1 downto 0)
     );
 end entity mux2para1;
 
